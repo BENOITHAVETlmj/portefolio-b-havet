9172c49aa0d86f73661759c53e6b486095d53b23
BENOITHAVETlmj <myungjae84@gmail.com>
2025-10-18 17:06:34 +0200

    fix webpack build error

--- src/components/pages/HomeTemplate/HomeTemplate.tsx
+++ src/components/pages/HomeTemplate/HomeTemplate.tsx
@@ -5,7 +5,7 @@ import { Hero } from "../../sections/Hero/Hero";
 import { Head } from "../../layouts/Head/Head";
 import Skills from "../../sections/Skills/Skills";
 import TechBanner from "../../TechBanner/TechBanner";
-import CvOverlay from "../../CVOverlay/CVOverlay";
+import CvOverlay from "../../CVOverlay/CvOverlay";
 
 const HomeTemplate = () => {
   return (
